0c1c56aeea00a84e9b6a97d8ecb4ef19a6853da9
Mike Mannix <mike.mannix@intuit.com>
2025-11-18 12:57:27 -0500

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import compress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://firecrackersohio.com",
+  integrations: [sitemap(), compress()],
 
   // Build optimizations
   build: {
